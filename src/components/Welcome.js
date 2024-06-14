d74ace9f5f25712d1254bf2467faca68d4069099
ReactJS_withDatThanh <datt2303@gmail.com>
2024-06-14 16:39:23 +0700

    mastermenoroi

---
+++ src/components/Welcome.js
@@ -0,0 +1,9 @@
+//Import react vào trong dự án
+// Welcome.js
+import React from 'react';
+
+function Welcome() {
+    return <h1>Đạt Thành đã bắt đầu học reactJS !</h1>;
+}
+
+export default Welcome;
